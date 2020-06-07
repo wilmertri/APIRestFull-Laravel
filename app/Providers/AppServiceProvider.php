8afa72b04257714f0df3cab6d878ce407013144f
Wilmer Triana <fabiantriana1072@gmail.com>
2020-06-06 19:22:31 -0500

    Se implementa las operaciones complejas para Product P-25

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use App\Product;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,6 +16,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        Product::updated(function($product){
+            if($product->quantity == 0 && $product->estaDisponible())
+            {
+                $product->status = Product::PRODUCTO_NO_DISPONIBLE;
+                $product->save();
+            }
+        });
     }
 
     /**
